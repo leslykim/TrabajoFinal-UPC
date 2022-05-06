a5d890f764df068464691b7551fb964dee4ef035
leslykim <42583853+leslykim@users.noreply.github.com>
2022-05-06 12:03:37 -0500

    Update TrabajoFinal01.java

--- src/Arreglo/TrabajoFinal01.java
+++ src/Arreglo/TrabajoFinal01.java
@@ -27,11 +27,13 @@ public class TrabajoFinal01 {
         public static double Weight = 0.00;
         public static double VolumenMaxCar = 0.00;
         public static double WeightMaxCar = 0.00;
+        //Para almacenar el cpontenido registrado
         public static ArrayList<Double> VolumenList = new ArrayList<>();
         public static ArrayList<Double> CostList = new ArrayList<>();
         public static ArrayList<Double> WeightList = new ArrayList<>();
 
         public static void main(String[] args) {
+                // proceso de Impresion 
             VolumenMaxCar = getInKeyboard("Ingrese volumen (M3) del auto :");
             WeightMaxCar = getInKeyboard("Ingrese Peso Maximo (KG) del auto :");
             int nextMenu = 0;
@@ -48,6 +50,7 @@ public class TrabajoFinal01 {
                     finalValue = getCostCalculateLetter(city);
                 }
                 else{
+                        //Para aceptar los valores de Teclado
                     Weight = getInKeyboard("Ingrese Peso (KG): ");
                     Height = getInKeyboard("Ingrese Altura (CM): ");
                     Large = getInKeyboard("Ingrese Largo (CM): ");
@@ -58,12 +61,13 @@ public class TrabajoFinal01 {
                     System.out.print("Costo Envio: ");
                     System.out.println(finalValue);
                 }else{
+                        //para imprimir los valores del Auto
                     System.out.println("Paquete excede capacidades maximas del auto");
                     System.out.println("Capacidad de auto ");
                     System.out.println(String.format("- M3 : %s / %s (%s", getArrayListTotal(VolumenList), VolumenMaxCar, getPorcentage(getArrayListTotal(VolumenList),VolumenMaxCar)) + "%)");
                     System.out.println(String.format("- KG : %s / %s (%s", getArrayListTotal(WeightList), WeightMaxCar, getPorcentage(getArrayListTotal(WeightList),WeightMaxCar)) + "%)");
                 }
-
+//Reseteamos los valores a 0
                 ResetValues();
                 nextMenu = printMenuType(MenuOptions, "¿Desea ingresar otro envio?") - 1;
             }
@@ -82,19 +86,21 @@ public class TrabajoFinal01 {
             // System.out.print("Costo total de envios: ");
             // System.out.println(getArrayListTotal(CostList));
         }
-
+ //Calculamos el costo del Sobre
         public static double getCostCalculateLetter(int index){
             VolumenList.add(0.00);
             CostList.add(CostBaseLetter[index]);
             return CostBaseLetter[index];
         }
-
+//Costos del paquete
         public static double getCostCalculatePackage(int index){
             //double weightVol = (Width * Height * Large)/6000.00;
+                //Formula Volumen Peso = Ancho x Largo / 6000.00
             double vol = (Width/100 * Height/100 * Large/100);
             double finalPrice = CostBasePackage[index];
 
             //double maxWeight = Weight > weightVol ? Weight : weightVol;
+                //Formula de Volumen Max = peso
             double maxWeight = Weight;
             maxWeight = maxWeight > WeightBasePackage[index] ? maxWeight - WeightBasePackage[index] : 0;
 
@@ -111,7 +117,7 @@ public class TrabajoFinal01 {
 
             return finalPrice;
         }
-
+// Almacenador del costo del (volumen y el precio) llenando en una variable
         public static double getArrayListTotal(ArrayList<Double> List){
             double total = 0.00;
             for (double vol: List) {
@@ -119,15 +125,15 @@ public class TrabajoFinal01 {
             }
             return total;
         }
-
+//porcentaje total
         public static double getPorcentage(double quantity, double total){
             return ((quantity*100)/ total);
         }
-
+//Limite del Carro
         public static boolean verifyLimitCar(double volumen, double weight){
             return ( (getArrayListTotal(VolumenList) + volumen) > VolumenMaxCar  || getArrayListTotal(WeightList) + weight > WeightMaxCar );
         }
-
+//el resulta obtenido estara en el tipo que debe ser menor a la opcion obtenida
         public static int printMenuType(String [] Options, String title){
             int result;
             do{
@@ -143,7 +149,7 @@ public class TrabajoFinal01 {
             }while (Options.length < result && result > 0);
             return result;
         }
-
+// datos ingresados del teclado 
         public static double getInKeyboard(String message){
             double result;
             Scanner scanner = new Scanner(System.in);
@@ -151,7 +157,7 @@ public class TrabajoFinal01 {
             result = scanner.nextDouble();
             return result;
         }
-
+//resetear los valores de peso y volumen del paquete o sobre
         public static void ResetValues() {
             Width = 0.00;
             Height = 0.00;
